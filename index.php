1224f9f7a64a321be108b7bac6678571cf528a5f
Brittany jack <jackbrittany54@yahoo.com>
2018-12-07 09:04:38 -0700

    flex-box sticky footer try-out code

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Food Truck Details</title>
+		<link rel="stylesheet" href="./styles/style.css">
+	</head>
+
+	<!--TODO center text in h1 nad h2-->
+	<body>
+		<!--container class for the header h1 and h2-->
+		<!--class center not working-->
+		<div class="center">
+			<h1>Sanchez Food Trucks</h1>
+			<h2>Address: 8020 Central Ave SE, Albuquerque, NM 87108</h2>
+			<p><strong>Open Hours: <br>Mon-Thrus 5pm-12am<br>
+					Fri-Sun 4:15pm-2:00am</strong></p>
+
+			<!--div container for images-->
+			<div class="container">
+				<img src="sanchez-tacosn%20(2).jpg" alt="food-truck-1" width="auto" height="400">
+				<!-- TODO need to add real description of Truck-->
+				<!--div class container for food truck description-->
+				<div class="container">
+					<h3><strong>Description</strong></h3>
+					<p>Bacon ipsum dolor amet frankfurter tongue corned beef salami. Fatback kevin pork loin meatloaf, jerky turducken ribeye.<br> Fatback ribeye tri-tip drumstick beef. Meatball corned beef alcatra cow bacon tail jerky boudin frankfurter jowl venison shoulder.<br> Jerky jowl biltong ground round brisket salami pig ball tip bresaola kevin porchetta picanha. <br>
+						Andouille pork loin kevin pork. Corned beef biltong ball tip, ribeye landjaeger pancetta filet mignon spare ribs hamburger prosciutto shoulder.</p>
+				</div>
+				<!--TODO add menu and center text-->
+				<!--div for menu link if have one-->
+				<div>
+					<link rel="placeholder" href="https://placeholder">
+				</div>
+				<!--TODO footer is not working correctly and need to add social media icons-->
+				<!--div for footer with social media links-->
+				<footer>
+					<div class="container">
+						<h4>Come visit us soon!</h4>
+					</div>
+				</footer>
+			</div>
+		</div>
+
+	</body>
+</html>
